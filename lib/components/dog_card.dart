212647a25d2665d21e77262fa7927bc346734712
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-22 15:22:09 -0500

    Removed old TODOs

--- lib/components/dog_card.dart
+++ lib/components/dog_card.dart
@@ -10,30 +10,6 @@ class DogCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO (1): Delete the Container.
-    // TODO (2): Return Card
-    // TODO (3): Card.child = ListTile
-    // TODO (5): ListTile.leading = CachedNetworkImage()
-
-    // TODO (6): CachedNetworkImage.width = 100
-    // TODO (7): CachedNetworkImage.fit = BoxFit.cover
-    // TODO (8): CachedNetworkImage.imageUrl = dog.imageUrls[0]
-    // TODO (9): CachedNetworkImage.placeholder = Centered CircularProgressIndicator of size 25x25
-    // placeholder: (context, url) => Center(
-    //   child: Container(
-    //     height: 25.0,
-    //     width: 25.0,
-    //     child: CircularProgressIndicator(),
-    //   ),
-    // ),
-    // TODO (10): CachedNetworkImage.errorWidget = Icon(Icons.broken_image)
-    // errorWidget: (context, url, error) => Icon(Icons.broken_image),
-
-    // TODO (11): ListTile.title = Text(dog.name)
-    // TODO (12): ListTile.subtitle = Text(dog.quote)
-
-    // TODO (13): Wrap the ListTile with a symmetric vertical padding of 20.0
-
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10.0),
