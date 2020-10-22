0434334f2e14b68c1b75c0e199eeaab2dc997ed9
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-22 17:10:13 -0500

    Removed old TODOs

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -62,72 +62,6 @@ class DetailsScreen extends StatelessWidget {
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-
-              // TODO (1): Padding.child = Column()
-
-              // Within Column.children
-              // TODO (2): Add a Text() for the Dog's name
-              // TODO (3): Text.textAlign = TextAlign.center
-              // TODO (4): Text.style = 30.0, FontWeight.w600
-              // Text(
-              //   dog.name ?? '-',
-              //   textAlign: TextAlign.center,
-              //   style: TextStyle(
-              //     fontSize: 30.0,
-              //     fontWeight: FontWeight.w600,
-              //   ),
-              // ),
-
-              // TODO (5): Add a SizedBox of height 5.0
-
-              // TODO (6): Add a Text() for the Dog's quote
-              // TODO (7): Text.textAlign = TextAlign.center
-              // TODO (8): Text.style = 18.0, FontWeight.w300
-              // Text(
-              //   dog.quote ?? '-',
-              //   textAlign: TextAlign.center,
-              //   style: TextStyle(
-              //     fontSize: 18.0,
-              //     fontWeight: FontWeight.w300,
-              //   ),
-              // ),
-
-              // TODO (9): Add a SizedBox of height 20.0
-
-              // TODO (11): Add a Row for the Dog's origin
-              // Within Row.children
-              // TODO (12): Icon(md_globe_icon) | SizedBox(width: 10.0) | Text
-              // TODO (13): Text.style = 18.0
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     Icon(FlutterIcons.md_globe_ion),
-              //     const SizedBox(
-              //       width: 10.0,
-              //     ),
-              //     Text(
-              //       dog.origin ?? '-',
-              //       style: TextStyle(fontSize: 18.0),
-              //     )
-              //   ],
-              // ),
-
-              // TODO (14): Add a SizedBox of height 20.0
-
-              // TODO (15): Add a Row for the height and weight
-
-              // Within Row.children
-
-              // 1. TODO (16): Create a Row for the height
-              // TODO (17): Icon(height) | SizedBox(width: 10.0) | Text(dog.height != null ? '${dog.height} ft' : '-')
-              // TODO (18): Text.style = 18.0
-
-              // 2. TODO (19): Create a Row for the weight
-              // TODO (20): Icon(weight) | SizedBox(width: 10.0) | Text(dog.weight != null ? '${dog.weight} ft' : '-')
-              // TODO (21): Text.style = 18.0
-
-              // TODO (22): For the outer most Row, set mainAxisAlignment = MainAxisAlignment.spaceEvenly
-
               child: Column(
                 children: [
                   Text(
@@ -200,7 +134,6 @@ class DetailsScreen extends StatelessWidget {
                   ),
                 ],
               ),
-
             )
           ],
         ),
